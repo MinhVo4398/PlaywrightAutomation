7cf13aed7c8046a721fd25f95c55c223e325dc2c
MinhVo <minhvo4398@gmail.com>
2023-09-24 19:13:29 +0700

    handle windows child

--- tests/UIBasicTest.spec.js
+++ tests/UIBasicTest.spec.js
@@ -34,11 +34,12 @@ test('Browser Context Playwright test', async ({ browser }) => {
 
 });
 
-test.only('UI Controls Playwright test', async ({ page }) => {
+test('UI Controls Playwright test', async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     const userName = page.locator("#username");
     const signIn = page.locator("#signInBtn");
     const dropdown = page.locator("select.form-control");
+    const documentLink = page.locator("[href*='documents-request']");
     await dropdown.selectOption("consult");
     await page.locator(".radiotextsty").last().click()
     await page.locator("#okayBtn").click();
@@ -50,10 +51,39 @@ test.only('UI Controls Playwright test', async ({ page }) => {
 
     await page.locator("#terms").uncheck();
     expect(await page.locator("#terms").isChecked()).toBeFalsy(); //true
-    
+
+    await expect(documentLink).toHaveAttribute("class", "blinkingText1");
+
+    //   await page.pause();
+
+});
+
+test.only('Child windows handle', async ({ browser }) => {
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const userName = page.locator("#username");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator("[href*='documents-request']");
+
+    const [newPage] = await Promise.all([
+
+        context.waitForEvent('page'),
+        documentLink.click(),
+    ])
+
+    const text = await newPage.locator(".red").textContent();
+    const arrayText = text.split("@")
+    const domain = arrayText[1].split(" ")[0]
+    console.log(domain);
+    await page.locator("#username").fill(domain);
+    await page.pause();
+    console.log(await page.locator("#username").textContent());
 
 
- //   await page.pause();
 
 
 });
+
+
+
