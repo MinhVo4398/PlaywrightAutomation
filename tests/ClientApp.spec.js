c8cc6f1a7155b86be2be566dac5aa20ffb09367d
MinhVo <minhvo4398@gmail.com>
2023-09-27 15:10:13 +0700

    auto suggestive dropdown

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -4,7 +4,7 @@ test("Client App login", async ({ page }) => {
     const email = "minh2@gmail.com";
     const productName = "Zara Coat 3";
     const products = page.locator(".card-body");
- 
+
     await page.goto("https://rahulshettyacademy.com/client");
     await page.locator("#userEmail").fill(email);
     await page.locator("#userPassword").fill("Minh@040398");
@@ -13,7 +13,7 @@ test("Client App login", async ({ page }) => {
     const titles = await page.locator(".card-body b").allTextContents();
     console.log(titles);
     const count = await products.count(); // this will tell the number of products in the products array
- 
+
     for (let i = 0; i < count; ++i) {
         if ((await products.nth(i).locator("b").textContent()) === productName);
         {
@@ -26,5 +26,21 @@ test("Client App login", async ({ page }) => {
     await page.locator("div li").first().waitFor(); // this will wait for the orders to load
     const bool = await page.locator("h3:has-text('Zara Coat 3')").isVisible();
     expect(bool).toBeTruthy();
- 
+    await page.locator("text=Checkout").click();
+    await page.locator("[placeholder='Select Country']").fill("ind", { delay: 100 });
+    const dropdown = page.locator(".ta-results");
+    // await dropdown.waitFor(5000); // hàm này k work
+    await page.waitForTimeout(5000)
+    const optionsCoount = await dropdown.locator("button").count();
+    for (let i = 0; i < optionsCoount; ++i) {
+        text = await dropdown.locator("button").nth(i).textContent();
+        if (text === " India") {
+            await dropdown.locator("button").nth(i).click();
+            break;
+        }
+    }
+
+    //await page.pause();
+
+
 });
